522caa2425d1889f7643324161faa3f0d70cd0ed
michael dilulo <snoleopard01001101@gmail.com>
2020-11-19 12:53:17 -0800

    Start over with Application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
-      <Row title="Top Rated" />
-      <Row title="Action Movies" />
-      <Row title="Comedy Movies" />
-      <Row title="Romance Movies" />
-      <Row title="Documentaries" />
+      <h1>Application Clone</h1>
     </div>
   );
 }

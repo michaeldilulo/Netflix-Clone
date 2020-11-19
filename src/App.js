6e879506791a93d730a8b359dae2de783395fcb5
michael dilulo <snoleopard01001101@gmail.com>
2020-11-19 11:42:53 -0800

    Titles render to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+      <Row title="Top Rated" />
+      <Row title="Action Movies" />
+      <Row title="Comedy Movies" />
+      <Row title="Romance Movies" />
+      <Row title="Documentaries" />
     </div>
   );
 }

532d947e41b0e26b6d75733276d443209c1764cf
michael dilulo <snoleopard01001101@gmail.com>
2020-11-20 12:09:10 -0800

    Scroll bar is hidden, but functionality is there

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios'
+import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -24,7 +25,8 @@ function Row({ title, fetchUrl }) {
             <h2>{title}</h2>
             <div className="row__posters">
                 {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    <img className="row__poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
             </div>
         </div>

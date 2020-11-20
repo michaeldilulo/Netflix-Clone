08de0d8ae31117c490b525b6374131cf3ac1ef67
michael dilulo <snoleopard01001101@gmail.com>
2020-11-20 11:43:15 -0800

    Request returns back to me in Console

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios'
 
-function Row(props) {
+
+function Row({ title, fetchUrl }) {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            // fetchUrl.setHeader('Access-Control-Allow-Origin', '*')
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl])
+
     return (
         <div>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
         </div>
     )
 }

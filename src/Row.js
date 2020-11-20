5b53bdb21806ae8e579551926b288f1c7ce58023
michael dilulo <snoleopard01001101@gmail.com>
2020-11-20 11:54:04 -0800

    Images Render to screen

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios'
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 
 function Row({ title, fetchUrl }) {
 
@@ -14,11 +16,17 @@ function Row({ title, fetchUrl }) {
             return request
         }
         fetchData()
+        // any variable passed outside of block, needs to tell useEffect to use the variable. It is a dependency
     }, [fetchUrl])
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
         </div>
     )
 }

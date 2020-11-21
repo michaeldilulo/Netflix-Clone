0bd07e6dca966a2539f31a92cfe3e7eefbece295
michael dilulo <snoleopard01001101@gmail.com>
2020-11-21 12:16:27 -0800

    Banner, format adjustments

--- src/Banner.js
+++ src/Banner.js
@@ -42,7 +42,9 @@ function Banner() {
                 <h1 className="banner__description">
                     {truncate(movie?.overview, 150)}
                 </h1>
-        </div>
+            </div>
+            
+            <div className="banner--fadeBottom" />
             </header>
     )
 }

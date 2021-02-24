e5ae0fb93c742e33a0b2e0f064dbc32c1b49d036
michael dilulo <snoleopard01001101@gmail.com>
2021-02-24 10:23:32 -0700

    Removed Files, for total upgrade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import './App.css';
-import Row from './Row';
-import requests from './requests'
-import Banner from './Banner'
-import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
-      isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+<h1>Netflix Challenge Clone</h1>
     </div>
   );
 }

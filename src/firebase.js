8a054d7fe0a89dad1ef159ec86356446108fa8af
michael dilulo <snoleopard01001101@gmail.com>
2021-02-25 07:33:08 -0700

    Firebase Information

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCosrQN2BnmkjanTC4dAvMnpBwzjhWHBHE",
+  authDomain: "netflix-clone-16d3e.firebaseapp.com",
+  databaseURL: "https://netflix-clone-16d3e.firebaseio.com",
+  projectId: "netflix-clone-16d3e",
+  storageBucket: "netflix-clone-16d3e.appspot.com",
+  messagingSenderId: "232157381730",
+  appId: "1:232157381730:web:158fe173da2967587d3d3b"
+};
